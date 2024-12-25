650211571a016bcdcf4b399284f7e4ff38ff0c64
Manuel Proß <pro_manuel@yahoo.de>
2024-12-25 22:27:30 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
