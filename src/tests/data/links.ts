7b25702417c9e7ff557b17a38ecd92e829378427
Manuel Proß <pro_manuel@yahoo.de>
2024-12-26 17:20:33 +0100

    test: add test data for unit tests

---
+++ src/tests/data/links.ts
@@ -0,0 +1,7 @@
+import { NavBarLink } from "../../types";
+
+export const testLinks: NavBarLink[] = [
+  { label: "link1", ref: "linkRef1" },
+  { label: "link2", ref: "linkRef2" },
+  { label: "link3", ref: "linkRef3" },
+];
