474780128c960b1bfc3bf5d909071ea1ca79cce8
Manuel Proß <pro_manuel@yahoo.de>
2025-01-12 13:37:13 +0100

    wip: animate mobile nav

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -2,13 +2,11 @@ import Nav from "../nav";
 
 const Header = () => {
   return (
-    <header className="py-8">
-      <div className="container flex justify-between mx-auto">
-        <h1 className="text-4xl font-semibold">
-          Manuel<span className="text-tokyo-green">.</span>
-        </h1>
-        <Nav />
-      </div>
+    <header className="flex justify-between mx-auto">
+      <h1 className="text-4xl font-semibold">
+        Manuel<span className="text-tokyo-green">.</span>
+      </h1>
+      <Nav />
     </header>
   );
 };
--- src/components/layout/nav/mobile-nav/index.tsx
+++ src/components/layout/nav/mobile-nav/index.tsx
@@ -18,26 +18,32 @@ const MobileNav = ({ links }: MobileNavProps) => {
   };
   return (
     <>
-      <BurgerIcon onStatusChange={handleMenuChange} />
+      <div className="absolute right-0">
+        <BurgerIcon onStatusChange={handleMenuChange} />
+      </div>
       <AnimatePresence>
         {isMenuOpen && (
-          <motion.ul
+          <motion.div
+            className="pt-6"
             data-testid="mobile-nav"
             data-cy="mobile-nav-links"
             initial={{ opacity: 0, x: 20 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: 20 }}
           >
-            {links.map((link) => {
-              return (
-                <li key={link.label}>
-                  <a data-testid="navbar-link" href={link.ref}>
-                    {link.label}
-                  </a>
-                </li>
-              );
-            })}
-          </motion.ul>
+            <h2>Manuel.</h2>
+            <ul className="flex flex-col justify-center gap-4 h-full text-lg text-center pt-10">
+              {links.map((link) => {
+                return (
+                  <li className="" key={link.label}>
+                    <a data-testid="navbar-link" href={link.ref}>
+                      {link.label}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
         )}
       </AnimatePresence>
     </>
