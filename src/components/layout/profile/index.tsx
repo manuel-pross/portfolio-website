3e71cc4ca1c87fedfc4fd2dd28fd896570686c33
Manuel Proß <pro_manuel@yahoo.de>
2025-02-15 17:55:43 +0100

    fix: remove oval shape of profile img on certain breakpoints

--- src/components/layout/profile/index.tsx
+++ src/components/layout/profile/index.tsx
@@ -4,7 +4,7 @@ import Socials from "@/components/ui/socials";
 function Profile() {
   return (
     <article className="flex flex-col items-center xl:items-start gap-6 xl:gap-0 xl:flex-row xl:justify-between">
-      <div className="order-2 xl:order-1 xl:w-[800px] 2xl:w-full 2xl:max-w-[1000px]">
+      <div className="order-2 xl:order-1 xl:w-[550px] 2xl:w-[750px]">
         <h2 className="h4 text-center xl:text-left mb-2">Frontend Developer</h2>
         <h1 className="h1 text-center xl:text-left">
           Hallo ich bin <span className="text-tokyo-green">Manuel Proß</span>
