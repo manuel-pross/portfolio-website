3bfa60846ec84a639668f388a6858f408753603c
Manuel Proß <pro_manuel@yahoo.de>
2025-02-15 13:33:47 +0100

    feat: add animation

--- src/components/ui/profilePhoto/index.tsx
+++ src/components/ui/profilePhoto/index.tsx
@@ -1,14 +1,21 @@
 import imageUrl from "@/assets/images/profile_image_circular.png";
+import { motion } from "motion/react";
 
 function ProfilePhoto() {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+      }}
+    >
       <img
-        className="w-[298px] h-auto xl:w-[498px] my-0 mx-auto"
+        className="w-[298px] h-auto xl:w-[498px] my-0 mx-auto radial-gradient"
         src={imageUrl}
         alt="A personal photo of Manuel Proß"
       />
-    </>
+    </motion.div>
   );
 }
 
