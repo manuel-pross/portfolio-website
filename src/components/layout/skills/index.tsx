d517300b15f93da08ad50b042e07eec94f2be7bd
Manuel Proß <pro_manuel@yahoo.de>
2025-02-22 07:34:36 +0100

    feat: add tooltip and hover animation for skills

--- src/components/layout/skills/index.tsx
+++ src/components/layout/skills/index.tsx
@@ -12,9 +12,12 @@ function Skills({ content }: SkillsProps) {
       <ul className="grid grid-cols-2 sm:grid-cols-4 justify-items-center md:justify-items-normal gap-5 max-w-[700px]">
         {content.items.map((item) => (
           <li
-            className="block w-fit text-6xl p-10 md:p-12 bg-tokyo-storm rounded-xl"
+            className="group relative text-6xl p-10 md:p-12 bg-tokyo-storm rounded-xl hover:text-tokyo-green transition-all ease-in-out delay-150"
             key={item.id}
           >
+            <span className="absolute top-1 left-0 z-10 hidden opacity-0 group-hover:opacity-100 group-hover:block w-full text-lg text-center text-tokyo-white">
+              {item.name}
+            </span>
             {item.icon}
           </li>
         ))}
